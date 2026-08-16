c2cdbf246425ba993109744e8c01396d056e43b4
oritri98 <ismatsiddiquee98@gmail.com>
2026-08-16 23:25:46 +0600

    Add num_hands parameter

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,7 @@ from mediapipe.tasks.python import vision
 
 base_options = mp_python.BaseOptions(model_asset_path=os.path.abspath(os.path.join(os.path.dirname(__file__), "hand_landmarker.task")))
 options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=2,
                                        
                                        
 
